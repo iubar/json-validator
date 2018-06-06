02073d1dc719c5f0a031b3bfc5eaba5118eeede2
Ema001 <bertoleroemanuele@gmail.com>
2018-06-06 16:42:43 +0200

    The begin

--- src/main/java/it/iubar/json_validator/PoTestJsonValidator.java
+++ src/main/java/it/iubar/json_validator/PoTestJsonValidator.java
@@ -1,5 +1,8 @@
 package it.iubar.json_validator;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
@@ -7,7 +10,15 @@ import org.json.JSONTokener;
 
 public class PoTestJsonValidator {
 	
-	public static void main(String[] args) {
-		
+	public void main(String[] args) throws IOException {
+		try (InputStream inputSchema = getClass().getResourceAsStream("schema.json")) {
+			  JSONObject rawSchema = new JSONObject(new JSONTokener(inputSchema));
+			  Schema schema = SchemaLoader.load(rawSchema);
+			  
+			  InputStream inputData = getClass().getResourceAsStream("data01.json");
+			  JSONObject data = new JSONObject(new JSONTokener(inputData));
+			  schema.validate(data); // throws a ValidationException if this object is invalid
+			  System.out.print("RISULTATO: " );
+			}
 	}
 }
