125185b88b2e1baa150f3278e831664c35876fc5
Ema001 <bertoleroemanuele@gmail.com>
2018-06-13 17:18:47 +0200

    Test GPG key

--- src/main/java/it/iubar/json_validator/Validator.java
+++ src/main/java/it/iubar/json_validator/Validator.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.ValidationException;
 import org.everit.json.schema.loader.SchemaLoader;
